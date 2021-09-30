0fb8c7035ab7b075485c8b7bc60347d54c63f5f4
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-30 00:58:21 +0000

    Cargando en las tarjetas

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -9,16 +9,16 @@ export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
+		<div className="container d-flex">
 			{store.people.map((item, index) => {
 				return (
-					<div key={index} className="card" style="width: 18rem;">
-						<img src="..." className="card-img-top" alt="..." />
+					<div key={index} className="card" style={{ width: "18rem" }}>
+						<img src="https://via.placeholder.com/100x200" className="card-img-top" alt="..." />
 						<div className="card-body">
-							<h5 className="card-title">Nombres</h5>
+							<h5 className="card-title">Datos</h5>
 							<p className="card-text">{item.name}</p>
 							<a href="#" className="btn btn-primary">
-								Go somewhere
+								Go to....
 							</a>
 						</div>
 					</div>
